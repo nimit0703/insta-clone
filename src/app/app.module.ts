c0500d77a61ac5a41ba923f4d3057cc2f2b3e181
nimit0703 <nimit.190410107143@gmail.com>
2023-09-03 15:16:09 +0530

    bootstarp added,structure designed,title image add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LeftPaneComponent } from './left-pane/left-pane.component';
+import { RightPaneComponent } from './right-pane/right-pane.component';
+import { MiddlePaneComponent } from './middle-pane/middle-pane.component';
+import { NavComponent } from './left-pane/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftPaneComponent,
+    RightPaneComponent,
+    MiddlePaneComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/right-pane/right-pane.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-right-pane',
+  templateUrl: './right-pane.component.html',
+  styleUrls: ['./right-pane.component.css']
+})
+export class RightPaneComponent {
+
+}
