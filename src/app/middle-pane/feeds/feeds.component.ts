f1ba09dc48010933f24d0139a8443465fba76bd5
nimit0703 <nimit.190410107143@gmail.com>
2023-09-05 17:12:17 +0530

    added other-links in right-pan,demo video

--- src/app/middle-pane/feeds/feeds.component.ts
+++ src/app/middle-pane/feeds/feeds.component.ts
@@ -11,9 +11,9 @@ export class FeedsComponent {
   feeds:Feed[] = [
     new Feed("johndoe_123","Sydney, Australia","10m","p-1","55","better worls"),
     new Feed("sarah.smith_456","Tokyo, Japan","58m","p-2","47","live in peace"),
-    new Feed("mike_jones789","new Feed York City, USA","5hr","p-3","33","sometimes"),
+    new Feed("mike_jones789","new York City, USA","5hr","p-3","33","sometimes"),
     new Feed("emily.brown_22","Sydney, Australia","8hr","p-4","23","streo of my heart"),
-    new Feed("chris_wilson_007","Rio de Janeiro, Brazil","7d","p-5","12","long leavess"),
+    new Feed("chris_wilson_007","Taj mahal, India","7d","p-5","12","long leavess"),
     new Feed("lisa.johnson_123","Cape Town, South Africa","4d","p-6","1m","weekends "),
     new Feed("robert_miller_55","Venice, Italy","9d","p-7","34","parting hard"),
     new Feed("jennifer.davis_888","Cairo, Egypt","3w","p-5","2","best vibes"),
