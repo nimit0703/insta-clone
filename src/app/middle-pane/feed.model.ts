3967481ce0cfa49efd4b5199f80c84774a0ec5e5
nimit0703 <nimit.190410107143@gmail.com>
2023-09-05 16:21:45 +0530

    added feed component

--- src/app/middle-pane/feed.model.ts
+++ src/app/middle-pane/feed.model.ts
@@ -1,14 +1,18 @@
 export class Feed {
 public user:string
+public location:string
 public timeBefore:string
 public feed:string
 public likes:number
+public caption:string
 
-constructor(user:string, timeBefore:string, feed:string, likes:number  ){
+constructor(user:string,location:string ,timeBefore:string, feed:string, likes:number ,caption:string ){
     this.user = user;
     this.timeBefore = timeBefore;
     this.feed = feed;
     this.likes = likes;
+    this.location=location;
+    this.caption=caption;
 }
 
 }
