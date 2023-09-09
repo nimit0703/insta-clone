9d13b0877ffa335ef5c0287f0d7ea141288b8572
nimit0703 <nimit.190410107143@gmail.com>
2023-09-09 19:02:33 +0530

    changes in import paths

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LeftPaneComponent } from './left-pane/left-pane.component';
-import { RightPaneComponent } from './right-pane/right-pane.component';
-import { MiddlePaneComponent } from './middle-pane/middle-pane.component';
-import { NavComponent } from './left-pane/nav/nav.component';
-import { AccDescSmComponent } from './right-pane/acc-desc-sm/acc-desc-sm.component';
-import { StoriesComponent } from './middle-pane/stories/stories.component';
-import { FeedsComponent } from './middle-pane/feeds/feeds.component';
+
 import { RouterModule, Routes } from '@angular/router';
-import { FirstPageComponent } from './first-page/first-page.component';
 import { HomeComponent } from './components/home/home.component';
+import { LeftPaneComponent } from './components/left-pane/left-pane.component';
+import { RightPaneComponent } from './components/right-pane/right-pane.component';
+import { MiddlePaneComponent } from './components/middle-pane/middle-pane.component';
+import { NavComponent } from './components/left-pane/nav/nav.component';
+import { AccDescSmComponent } from './components/right-pane/acc-desc-sm/acc-desc-sm.component';
+import { StoriesComponent } from './components/middle-pane/stories/stories.component';
+import { FeedsComponent } from './components/middle-pane/feeds/feeds.component';
+import { FirstPageComponent } from './components/first-page/first-page.component';
 
 
 
